3fb84a98fd3ef1a4c206304c8064618b506f9f1b
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2026-05-02 00:14:54 +0330

    Try Another Way To Implement Bottom Navigation

--- core/ui/src/main/java/info/alihabibi/ui/navigation/BottomNavItem.kt
+++ core/ui/src/main/java/info/alihabibi/ui/navigation/BottomNavItem.kt
@@ -1,9 +1,15 @@
 package info.alihabibi.ui.navigation
 
-import androidx.compose.ui.graphics.painter.Painter
+import info.alihabibi.designsystem.R
 
-data class BottomNavItem(
-    val route: String?,
-    val label: String,
-    val icon: Painter
-)
+enum class BottomNavItem(val labelResId: Int, val iconResId: Int) {
+
+    HOME(R.string.home, R.drawable.home),
+
+    PROFILE(R.string.profile, R.drawable.profile),
+
+    REMINDER(R.string.reminder, R.drawable.reminder),
+
+    REPORTS(R.string.reports, R.drawable.reports);
+
+}
