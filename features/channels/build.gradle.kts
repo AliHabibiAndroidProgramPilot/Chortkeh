2352ada74f66c3540375f4cd33d7d1de6cccf14b
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2026-08-28 16:58:05 +0330

    Add Channels Module

---
+++ features/channels/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("info.alihabibi.chortkeh.android.library")
+    id("info.alihabibi.chortkeh.android.compose")
+}
+
+android {
+    namespace = "info.alihabibi.channels"
+}
+
+dependencies {
+
+    // design system
+    implementation(projects.core.designsystem)
+
+    // ui
+    implementation(projects.core.ui)
+
+    // domain
+    implementation(projects.core.domain)
+
+    // model
+    implementation(projects.core.model)
+
+}
