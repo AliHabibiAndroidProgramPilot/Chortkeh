c5527101396abb9b9a7560a04fc991f693cc6b83
AliHabibiAndroidProgramPilot <biz.alihabibi@gmail.com>
2026-09-01 18:44:42 +0330

    Add Read SMS Switch For Channels

--- features/channels/src/main/java/info/alihabibi/channels/screens/AddChannel.kt
+++ features/channels/src/main/java/info/alihabibi/channels/screens/AddChannel.kt
@@ -3,6 +3,7 @@ package info.alihabibi.channels.screens
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -20,6 +21,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -36,6 +39,9 @@ import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDirection
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,9 +52,12 @@ import info.alihabibi.channels.ChannelsViewModel
 import info.alihabibi.designsystem.R
 import info.alihabibi.designsystem.theme.Gray11
 import info.alihabibi.designsystem.theme.Gray3
+import info.alihabibi.designsystem.theme.Gray5
 import info.alihabibi.designsystem.theme.Gray6
+import info.alihabibi.designsystem.theme.Gray7
 import info.alihabibi.designsystem.theme.Gray8
 import info.alihabibi.designsystem.theme.Primary
+import info.alihabibi.designsystem.theme.White
 import info.alihabibi.model.ui_model.channel.ChannelIconOptionUiModel
 import info.alihabibi.ui.buttons.AppButton
 import info.alihabibi.ui.dialogs.AppIconSelectionBottomSheet
@@ -280,7 +289,46 @@ private fun BankAccountChannelContent(
             title = stringResource(id = R.string.source_name)
         )
 
-        //todo add sms switch check in figma file
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+
+            Switch(
+                checked = true,
+                onCheckedChange = {
+
+                },
+                colors = SwitchDefaults.colors(
+                    checkedThumbColor = White,
+                    uncheckedThumbColor = White,
+                    checkedTrackColor = Primary,
+                    uncheckedTrackColor = Gray5,
+                    uncheckedBorderColor = Gray5
+                )
+            )
+
+            Text(
+                modifier = Modifier
+                    .weight(weight = 1f)
+                    .offset(y = 8.dp),
+                text = stringResource(id = R.string.read_sms),
+                style = MaterialTheme.typography.bodyLarge
+            )
+
+        }
+
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(end = 16.dp),
+            text = stringResource(id = R.string.read_sms_description),
+            style = MaterialTheme.typography.labelMedium.copy(color = Gray7, textAlign = TextAlign.Right, textDirection = TextDirection.Rtl),
+            overflow = TextOverflow.Ellipsis
+        )
 
     }
 
